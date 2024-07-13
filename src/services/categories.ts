7c2635049d561a522e26271041c171679c226a93
Mati Cassanelli <matiascassanelli@gmail.com>
2024-07-13 10:44:56 -0300

    Add categories service file

---
+++ src/services/categories.ts
@@ -0,0 +1,23 @@
+import { db } from '../firebase/config';
+import { Category } from '../types/Transaction';
+import {
+  collection as firestoreCollection,
+  query as firestoreQuery,
+  getDocs,
+} from 'firebase/firestore';
+
+const collectionName = 'categories';
+const collection = firestoreCollection(db, collectionName);
+
+export const getAllCategories = async () => {
+  try {
+    const q = firestoreQuery(collection);
+    const documents = await getDocs(q);
+    return documents.docs.map((x) => ({
+      ...(x.data() as Category),
+      id: x.id,
+    }));
+  } catch (error) {
+    throw error;
+  }
+};
