e29aa8d26963ba9659355963177a2dfaa46d7c01
Mati Cassanelli <matiascassanelli@gmail.com>
2024-07-13 10:44:28 -0300

    Add library to handle dates and calendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,32 @@
+import { LocalizationProvider } from '@mui/x-date-pickers';
 import './App.css';
 import { Routes } from './Routes';
 import UserProvider from './context/UserContext';
 import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
 const App = () => {
+  const customTheme = createTheme({
+    components: {
+      MuiDialogContent: {
+        styleOverrides: {
+          root: {
+            '&.margined': {
+              paddingTop: '8px',
+            },
+          },
+        },
+      },
+    },
+  });
   return (
-    <ThemeProvider theme={createTheme()}>
+    <ThemeProvider theme={customTheme}>
       <CssBaseline />
-      <UserProvider>
-        <Routes />
-      </UserProvider>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <UserProvider>
+          <Routes />
+        </UserProvider>
+      </LocalizationProvider>
     </ThemeProvider>
   );
 };
