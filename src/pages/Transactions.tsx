7dc13709038e21e265dc3656749646472971ddb7
Mati Cassanelli <matiascassanelli@gmail.com>
2024-07-13 10:48:27 -0300

    Rewrite transaction service file to get rid of db file

--- src/pages/Transactions.tsx
+++ src/pages/Transactions.tsx
@@ -1,8 +1,8 @@
 import { CircularProgress, Container, Typography } from '@mui/material';
 import { Transaction } from '../types/Transaction';
-import { useCallback, useContext, useEffect, useState } from 'react';
-import { FirestoreError, where } from 'firebase/firestore';
-import { getAllTransactions } from '../services/transactions';
+import { useContext, useEffect, useState } from 'react';
+import { where } from 'firebase/firestore';
+import { getTransactionsSnapshot } from '../services/transactions';
 import { UserContext } from '../context/UserContext';
 import { TransactionList } from '../components/TransactionList';
 
@@ -11,23 +11,26 @@ export const Transactions = () => {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
   const [error, setError] = useState('');
   const [loading, setLoading] = useState(false);
-  const getTransactions = useCallback(async () => {
-    setLoading(true);
-    try {
-      const response = await getAllTransactions([
-        where('userId', '==', user?.uid),
-      ]);
-      setTransactions(response as Transaction[]);
-      setError('');
-    } catch (error) {
-      setError((error as FirestoreError)?.message);
-    }
-    setLoading(false);
-  }, [user?.uid]);
 
   useEffect(() => {
-    getTransactions();
-  }, [getTransactions]);
+    setLoading(true);
+    const unsubscribe = getTransactionsSnapshot({
+      onSuccess: (querySnapshot) => {
+        const docs = querySnapshot.docs.map((x) => ({
+          ...(x.data() as Transaction),
+          id: x.id,
+        }));
+        setTransactions(docs);
+        setLoading(false);
+      },
+      onError: (error) => {
+        setError(error.message);
+        setLoading(false);
+      },
+      filters: [where('userId', '==', user?.uid)],
+    });
+    return () => unsubscribe();
+  }, [user?.uid]);
 
   if (loading) {
     return <CircularProgress />;
--- src/services/transactions.ts
+++ src/services/transactions.ts
@@ -1,22 +1,52 @@
-import { orderBy, QueryFieldFilterConstraint } from 'firebase/firestore';
-import { getDocuments } from '../firebase/db';
+import {
+  collection as firestoreCollection,
+  query as firestoreQuery,
+  QueryFieldFilterConstraint,
+  onSnapshot,
+  QuerySnapshot,
+  orderBy,
+  FirestoreError,
+  addDoc,
+  getDocs,
+} from 'firebase/firestore';
 import { Transaction } from '../types/Transaction';
+import { db } from '../firebase/config';
 
 const collectionName = 'transactions';
+const collection = firestoreCollection(db, collectionName);
+
+export const getTransactionsSnapshot = ({
+  filters = [],
+  onSuccess,
+  onError,
+}: {
+  onSuccess: (querySnapshot: QuerySnapshot) => void;
+  onError?: (error: FirestoreError) => void;
+  filters?: QueryFieldFilterConstraint[];
+}) => {
+  const query = firestoreQuery(collection, ...filters, orderBy('date', 'desc'));
+  return onSnapshot(query, onSuccess, onError);
+};
 
 export const getAllTransactions = async (
   query: QueryFieldFilterConstraint[] = []
 ) => {
   try {
-    const documents = await getDocuments(
-      collectionName,
-      query,
-      orderBy('date', 'desc')
-    );
+    const q = firestoreQuery(collection, ...query, orderBy('date', 'desc'));
+    const documents = await getDocs(q);
     return documents.docs.map((x) => ({
       ...(x.data() as Transaction),
       id: x.id,
     }));
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const createTransaction = async (transaction: Transaction) => {
+  try {
+    const document = await addDoc(collection, transaction);
+    return { ...transaction, id: document.id };
   } catch (error) {
     throw error;
   }
