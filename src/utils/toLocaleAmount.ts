60a91d38de4c26f04521c0b9f4eeef013025963f
Mati Cassanelli <matiascassanelli@gmail.com>
2024-07-29 16:24:43 -0300

    Show more decimal digits

--- src/utils/toLocaleAmount.ts
+++ src/utils/toLocaleAmount.ts
@@ -1,6 +1,7 @@
 export const toLocaleAmount = (amount: number | string) => {
   const stringAmount = amount.toLocaleString('es-ar', {
     minimumFractionDigits: 2,
+    maximumFractionDigits: 8
   });
   return stringAmount;
 };
