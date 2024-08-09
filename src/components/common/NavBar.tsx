97cd59468a9e6b56c71af17f6ebaa925610aeb84
Mati Cassanelli <matiascassanelli@gmail.com>
2024-08-09 12:43:14 -0300

    Improve navbar with media query

--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -11,6 +11,7 @@ import {
   ListItemText,
   Toolbar,
   Typography,
+  useMediaQuery,
   useTheme,
 } from '@mui/material';
 import { useState } from 'react';
@@ -27,6 +28,7 @@ export const NavBarLink = ({ to, children }: LinkProps) => {
   const { primary, background, common } = palette;
   const navigate = useNavigate();
   const match = useMatchPath(to);
+
   return (
     <Button
       onClick={() => {
@@ -76,7 +78,8 @@ export const MobileNavBarLink = ({
 };
 
 export const NavBar = () => {
-  const { palette } = useTheme();
+  const { palette, breakpoints } = useTheme();
+  const isMobileScreen = useMediaQuery(breakpoints.only('xs'));
   const [mobileOpen, setMobileOpen] = useState(false);
 
   const handleDrawerToggle = () => {
@@ -87,26 +90,31 @@ export const NavBar = () => {
     <>
       <AppBar component="nav">
         <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
-          <Box sx={{ display: 'flex', alignItems: 'center', gap: 3 }}>
-            <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              edge="start"
-              onClick={handleDrawerToggle}
-              sx={{ mr: 2, display: { sm: 'none' } }}
-            >
-              <Menu />
-            </IconButton>
-            <Box sx={{ display: { xs: 'none', sm: 'flex' } }}>
-              <img src={MioLogo} alt="logo" width={90} />
-            </Box>
-            <Box sx={{ display: { xs: 'none', sm: 'flex' }, gap: 2 }}>
-              {PAGES.map(({ name, url }) => (
-                <NavBarLink key={name} to={url}>
-                  {name}
-                </NavBarLink>
-              ))}
-            </Box>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            {isMobileScreen && (
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                edge="start"
+                onClick={handleDrawerToggle}
+              >
+                <Menu />
+              </IconButton>
+            )}
+            {!isMobileScreen && (
+              <>
+                <Box sx={{ display: 'flex', marginRight: 2 }}>
+                  <img src={MioLogo} alt="logo" width={90} />
+                </Box>
+                <Box sx={{ display: 'flex', gap: 1, marginRight: 1 }}>
+                  {PAGES.map(({ name, url }) => (
+                    <NavBarLink key={name} to={url}>
+                      {name}
+                    </NavBarLink>
+                  ))}
+                </Box>
+              </>
+            )}
           </Box>
           <Button
             variant="text"
@@ -118,31 +126,32 @@ export const NavBar = () => {
           </Button>
         </Toolbar>
       </AppBar>
-      <Drawer
-        open={mobileOpen}
-        onClose={handleDrawerToggle}
-        sx={{
-          display: { xs: 'block', sm: 'none' },
-          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: 180 },
-        }}
-      >
-        <Box
+      {isMobileScreen && (
+        <Drawer
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
           sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            paddingY: 1,
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: 180 },
           }}
         >
-          <img src={MioLogo} alt="logo" width={140} />
-        </Box>
-        <Divider />
-        <List>
-          {PAGES.map(({ name, url }) => (
-            <MobileNavBarLink key={name} to={url} name={name} />
-          ))}
-        </List>
-      </Drawer>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              paddingY: 1,
+            }}
+          >
+            <img src={MioLogo} alt="logo" width={140} />
+          </Box>
+          <Divider />
+          <List>
+            {PAGES.map(({ name, url }) => (
+              <MobileNavBarLink key={name} to={url} name={name} />
+            ))}
+          </List>
+        </Drawer>
+      )}
     </>
   );
 };
